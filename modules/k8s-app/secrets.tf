f3dca5f78ad1de52dff66f93efbf344b5753cb25
Manoel Carvalho <manoelhc@gmail.com>
2020-06-07 12:36:14 +0100

    Move secrets to apps

--- modules/k8s-app/secrets.tf
+++ modules/k8s-app/secrets.tf
@@ -1,19 +1,59 @@
-data "aws_ssm_parameter" "db-root-username" {
-  name = "/${var.name}/${var.environment}/database/root-username/master"
+resource "aws_ssm_parameter" "db-root-username" {
+  name        = "/${var.name}/${var.environment}/database/root-username/master"
+  description = "${var.name} ${var.environment}'s Database Username"
+  type        = "SecureString"
+  value       = "${base64sha256("${timestamp()}-root-username")}"
+
+  tags = {
+    appname     = "${var.name}"
+    environment = "${var.environment}"
+  }
 }
 
-data "aws_ssm_parameter" "db-root-password" {
-  name = "/${var.name}/${var.environment}/database/root-password/master"
+resource "aws_ssm_parameter" "db-root-password" {
+  name        = "/${var.name}/${var.environment}/database/root-password/master"
+  description = "${var.name} ${var.environment}'s Database Password"
+  type        = "SecureString"
+  value       = "${base64sha256("${timestamp()}-root-password")}"
+
+  tags = {
+    appname     = "${var.name}"
+    environment = "${var.environment}"
+  }
 }
 
-data "aws_ssm_parameter" "db-dbname" {
-  name = "/${var.name}/${var.environment}/database/dbname/master"
+resource "aws_ssm_parameter" "db-dbname" {
+  name        = "/${var.name}/${var.environment}/database/dbname/master"
+  description = "${var.name} ${var.environment}'s Database DB Name"
+  type        = "SecureString"
+  value       = "${base64sha256("${timestamp()}-root-dbname")}"
+
+  tags = {
+    appname     = "${var.name}"
+    environment = "${var.environment}"
+  }
 }
 
-data "aws_ssm_parameter" "db-app-username" {
-  name = "/${var.name}/${var.environment}/database/username/master"
+resource "aws_ssm_parameter" "db-app-username" {
+  name        = "/${var.name}/${var.environment}/database/username/master"
+  description = "${var.name} ${var.environment}'s Database Username"
+  type        = "SecureString"
+  value       = "${base64sha256("${timestamp()}-app-username")}"
+
+  tags = {
+    appname     = "${var.name}"
+    environment = "${var.environment}"
+  }
 }
 
-data "aws_ssm_parameter" "db-app-password" {
-  name = "/${var.name}/${var.environment}/database/password/master"
+resource "aws_ssm_parameter" "db-app-password" {
+  name        = "/${var.name}/${var.environment}/database/password/master"
+  description = "${var.name} ${var.environment}'s Database Password"
+  type        = "SecureString"
+  value       = "${base64sha256("${timestamp()}-app-password")}"
+
+  tags = {
+    appname     = "${var.name}"
+    environment = "${var.environment}"
+  }
 }
