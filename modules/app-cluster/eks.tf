418ea65becaebd286f2b3b53ea4fd2f2a98d9603
Manoel Carvalho <manoelhc@gmail.com>
2020-06-07 12:20:45 +0100

    Remvoe old comment

--- modules/app-cluster/eks.tf
+++ modules/app-cluster/eks.tf
@@ -14,9 +14,6 @@ resource "aws_eks_cluster" "this" {
     subnet_ids = local.app_subnet_ids
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-
   depends_on = [
     "aws_cloudwatch_log_group.this",
   ]
